46877047e5bcf2dbb0bc4993dde8acee4ea4a104
Margus Liinev <margus.liinev@gmail.com>
2024-03-08 23:28:33 +0200

    add graceful shutdown, statuscode, access-control rules

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ const server = http.createServer()
 server.on('request', (request, response) => {
     const result = fs.readFileSync('./text.txt')
 
+    response.statusCode = 200
+
     response.setHeader('Content-Type', 'text/plain')
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
     response.end(result)
 })
 
-server.listen(4080, '127.0.0.1', () => {
-    console.log('Server has started on:', server.address())
-})
+server
+    .listen(4080, '127.0.0.1', () => {
+        console.log('Server has started on:', server.address())
+    })
+    .on('error', (error) => {
+        console.log('Error:', error)
+        process.exit(1)
+    })
