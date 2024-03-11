bb1ecd350d96fe8124ac77de01703c1fe5d4626b
Margus Liinev <margus.liinev@gmail.com>
2024-03-11 18:13:21 +0200

    add create a file command

--- file-commander/app.ts
+++ file-commander/app.ts
@@ -1,6 +1,20 @@
 import fs from 'fs/promises';
 
 (async () => {
+    const CREATE_A_FILE = 'create a file';
+
+    const createFile = async (path: string) => {
+        try {
+            const existingFileHandle = await fs.open(path, 'r');
+            existingFileHandle?.close();
+            return console.log(`The file at ${path} already exists.`);
+        } catch (error) {
+            const newFileHandle = await fs.open(path, 'w');
+            console.log(`A new file was successfully created at ${path}`);
+            newFileHandle.close();
+        }
+    };
+
     const commandFileHandler = await fs.open('./command.txt', 'r');
 
     try {
@@ -14,8 +28,14 @@ import fs from 'fs/promises';
                 const offset = 0;
                 const position = 0;
 
-                const content = await commandFileHandler.read(buff, offset, length, position);
-                console.log(content);
+                await commandFileHandler.read(buff, offset, length, position);
+
+                const command = buff.toString('utf8');
+
+                if (command.includes('create a file')) {
+                    const filePath = command.substring(CREATE_A_FILE.length + 1);
+                    createFile(filePath);
+                }
             }
         }
     } catch (error) {
