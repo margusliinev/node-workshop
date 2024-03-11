ee4544461c5d8b59560a50d95baf29d869a54b80
Margus Liinev <margus.liinev@gmail.com>
2024-03-11 13:54:29 +0200

    watch file changes and log content, allocate exact buffer size

--- file-commander/app.ts
+++ file-commander/app.ts
@@ -1,10 +1,22 @@
-const fs = require('fs/promises');
+import fs from 'fs/promises';
 
 (async () => {
+    const commandFileHandler = await fs.open('./command.txt', 'r');
+
     try {
-        const watcher = fs.watch('./');
+        const watcher = fs.watch('./command.txt');
         for await (const event of watcher) {
-            console.log(event);
+            if (event.eventType === 'change') {
+                const size = (await commandFileHandler.stat()).size;
+                const buff = Buffer.alloc(size);
+
+                const length = size;
+                const offset = 0;
+                const position = 0;
+
+                const content = await commandFileHandler.read(buff, offset, length, position);
+                console.log(content);
+            }
         }
     } catch (error) {
         console.error('Error occurred:', error);
