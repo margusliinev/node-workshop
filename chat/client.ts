89fd6053e06d83a6d8f562c6187519e0dc75d29f
Margus Liinev <margus.liinev@gmail.com>
2024-03-13 14:42:03 +0200

    show all messages to each client, move cursor and clearline for new messages

--- chat/client.ts
+++ chat/client.ts
@@ -1,9 +1,50 @@
 import net from 'net';
+import { Direction } from 'readline';
+import readline from 'readline/promises';
 
-const client = net.createConnection({ host: '127.0.0.1', port: 5000 }, () => {
-    console.log('Connected to the server');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
 
-client.on('end', () => {
+const clearLine = (dir: Direction) => {
+    return new Promise<void>((resolve) => {
+        process.stdout.clearLine(dir, () => {
+            resolve();
+        });
+    });
+};
+
+const moveCursor = (dx: Direction, dy: Direction) => {
+    return new Promise<void>((resolve) => {
+        process.stdout.moveCursor(dx, dy, () => {
+            resolve();
+        });
+    });
+};
+
+const ask = async () => {
+    const message = await rl.question('Enter a message > ');
+    await moveCursor(0, -1);
+    await clearLine(0);
+    client.write(message);
+};
+
+const client = net.createConnection({ host: '127.0.0.1', port: 5000 }, async () => {
+    ask();
+});
+
+client.on('data', async (data) => {
+    console.log();
+    await moveCursor(0, -1);
+    await clearLine(0);
+    console.log(data.toString('utf8'));
+    ask();
+});
+
+client.on('end', async () => {
+    console.log();
+    await moveCursor(0, -1);
+    await clearLine(0);
     console.log('Disconnected from the server');
 });
--- chat/server.ts
+++ chat/server.ts
@@ -2,8 +2,19 @@ import net from 'net';
 
 const server = net.createServer();
 
+const clients: net.Socket[] = [];
+
 server.on('connection', (socket) => {
     console.log('New client connected to the server');
+    socket.on('data', (data) => {
+        console.log('Received data from port:', socket.remotePort);
+        console.log('Data:', data.toString('utf8'));
+        clients.map((client) => {
+            client.write(data);
+        });
+    });
+
+    clients.push(socket);
 });
 
 server.listen(5000, '127.0.0.1', () => {
