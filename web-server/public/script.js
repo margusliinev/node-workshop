ddb062604be16073df2d2c48b0238ef55e05bced
Margus Liinev <margus.liinev@gmail.com>
2024-03-17 15:00:12 +0200

    serve css,js,favicon files

---
+++ web-server/public/script.js
@@ -0,0 +1,3 @@
+let h2 = document.createElement('h2');
+h2.textContent = 'If you see this message, your JavaScript is working!';
+document.body.appendChild(h2);
--- web-server/server.ts
+++ web-server/server.ts
@@ -5,14 +5,38 @@ const server = http.createServer();
 const PORT = 5000;
 
 server.on('request', async (request, response) => {
-    if (request.url === '/' && request.method === 'GET') {
-        response.setHeader('Content-Type', 'text/html');
-        response.statusCode = 200;
-        const fileHandle = await fs.open('./public/index.html', 'r');
-        const fileStream = fileHandle.createReadStream();
-        fileStream.pipe(response);
+    switch (request.url) {
+        case '/':
+            await serveStaticFile(response, 'index.html', 'text/html');
+            break;
+        case '/style.css':
+            await serveStaticFile(response, 'style.css', 'text/css');
+            break;
+        case '/script.js':
+            await serveStaticFile(response, 'script.js', 'text/javascript');
+            break;
+        case '/favicon.ico':
+            await serveStaticFile(response, 'favicon.ico', 'image/x-icon');
+            break;
+        default:
+            response.statusCode = 404;
+            response.end();
     }
 });
+
+async function serveStaticFile(response: http.ServerResponse, path: string, contentType: string) {
+    try {
+        const fileHandle = await fs.open(`./public/${path}`, 'r');
+        const fileStream = fileHandle.createReadStream();
+        response.setHeader('Content-Type', contentType);
+        response.statusCode = 200;
+        fileStream.pipe(response);
+    } catch (error) {
+        console.error(error);
+        response.statusCode = 500;
+        response.end();
+    }
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
