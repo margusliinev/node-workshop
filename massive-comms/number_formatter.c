00777d6b444fb5aa11ffb6855bdc95448bbba6a0
Margus Liinev <margus.liinev@gmail.com>
2024-03-24 17:13:45 +0200

    add formatNumber func with comma separation and $ sign per number

--- massive-comms/number_formatter.c
+++ massive-comms/number_formatter.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+char *formatNumber(char *input, char begin, char divider)
+{
+    int length = strlen(input);
+    int formattedLength = length + length / 3 + 2;
+
+    char *formattedNumber = (char *)malloc(formattedLength * sizeof(char));
+
+    int j = 0;
+    int commaCount = length % 3;
+
+    formattedNumber[0] = begin;
+    j = j + 1;
+
+    for (int i = 0; i < length; i++)
+    {
+        formattedNumber[j] = input[i];
+        j = j + 1;
+
+        if (commaCount > 0 && i < length - 1 && (i + 1) % 3 == commaCount)
+        {
+            formattedNumber[j++] = divider;
+        }
+        else if (commaCount == 0 && i < length - 1 && (i + 1) % 3 == 0)
+        {
+            formattedNumber[j++] = divider;
+        }
+    }
+
+    formattedNumber[j] = '\0';
+
+    return formattedNumber;
+}
 
 int main(int argc, char *argv[])
 {
-
     FILE *outputFile = fopen(argv[1], "w");
     if (outputFile == NULL)
     {
@@ -13,7 +46,6 @@ int main(int argc, char *argv[])
 
     char *number = malloc(12 * sizeof(char));
     int index = 0;
-
     int c = fgetc(stdin);
 
     while (c != EOF)
@@ -25,11 +57,10 @@ int main(int argc, char *argv[])
         }
         else
         {
-            number[index] = '%';
-            number[++index] = '\0';
-            fprintf(outputFile, "%s ", number);
-            free(number);
-            number = malloc(12 * sizeof(char));
+            number[index] = '\0';
+            char *formattedNumber = formatNumber(number, argv[2][0], argv[3][0]);
+            fprintf(outputFile, "%s ", formattedNumber);
+            free(formattedNumber);
             index = 0;
         }
         c = fgetc(stdin);
@@ -37,12 +68,13 @@ int main(int argc, char *argv[])
 
     if (index > 0)
     {
-        number[index] = '%';
-        number[++index] = '\0';
-        fprintf(outputFile, "%s ", number);
-        free(number);
+        number[index] = '\0';
+        char *formattedNumber = formatNumber(number, argv[2][0], argv[3][0]);
+        fprintf(outputFile, "%s ", formattedNumber);
+        free(formattedNumber);
     }
 
     fclose(outputFile);
-    exit(0);
+    free(number);
+    return 0;
 }
