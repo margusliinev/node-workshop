8a0dffdb5590cdb1bafa9e31b9fc3c81e953cd18
Margus Liinev <margus.liinev@gmail.com>
2024-03-24 16:58:25 +0200

    add % to numbers, delete allocated memory after usage

--- massive-comms/number_formatter.c
+++ massive-comms/number_formatter.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    
+int main(int argc, char *argv[])
+{
+
     FILE *outputFile = fopen(argv[1], "w");
-    if (outputFile == NULL) {
+    if (outputFile == NULL)
+    {
         perror("Error opening file");
         return 1;
     }
 
-    char c = fgetc(stdin);
+    char *number = malloc(12 * sizeof(char));
+    int index = 0;
 
-    while (c != EOF) {
-        fprintf(outputFile, "%c", c);
-        fflush(outputFile);
+    int c = fgetc(stdin);
+
+    while (c != EOF)
+    {
+        if (c != ' ')
+        {
+            number[index] = c;
+            index++;
+        }
+        else
+        {
+            number[index] = '%';
+            number[++index] = '\0';
+            fprintf(outputFile, "%s ", number);
+            free(number);
+            number = malloc(12 * sizeof(char));
+            index = 0;
+        }
         c = fgetc(stdin);
+    }
+
+    if (index > 0)
+    {
+        number[index] = '%';
+        number[++index] = '\0';
+        fprintf(outputFile, "%s ", number);
+        free(number);
     }
 
     fclose(outputFile);
